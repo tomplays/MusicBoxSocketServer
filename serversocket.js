f00fe3cd57c4cefd94193586fbd66fecb9e44dc4
tomplays <homeof@gmail.com>
2014-01-28 08:53:58 +0100

    dynamic conf, no debug

--- serversocket.js
+++ serversocket.js
@@ -14,34 +14,42 @@ var app_package = require('./package.json'),
 nconf  = require('nconf');
 nconf.argv().env().file({file:'config.json'});
 
-var io     = require('socket.io').listen(nconf.get('PORT'));
+var io     = require('socket.io').listen(nconf.get('PORT'), { 
+                                                                log: false 
+                                                            });
 
 var users= 0;
 
+console.log('~:~:~~: Init socket.io @ '+nconf.get("SERVER_URL")+':'+nconf.get("PORT"));
+
 io.sockets.on('connection', function (socket) {
-        console.log('serversocket is on.. listening on '+nconf.get("PORT")+' port');
+
+
+        var address = socket.handshake.address;
+        console.log('~:~:~~: Handshake by remote client (' + address.address + ":" + address.port)+' )';
+
         if(users<1){
                 users=1;
         }
         users++;
 	// act as relay
         socket.on('news', function (data) {
-                console.log('news event', data);
+                console.log('~:~:~~: news event', data);
                 socket.broadcast.emit('news', data);
         });
         socket.on('disconnect', function () {
                 users--;
-                console.log('user left'+users);
+                console.log('~:~:~~: user left '+users);
                 socket.broadcast.emit('users_count', users);
         });
         socket.on('user_enter', function (data) {
                 users++;
-                console.log('new user'+users);
+                console.log('~:~:~~: new user '+users);
                 socket.broadcast.emit('users_count', users);
         });
         socket.on('user', function (data) {
-                console.log('user action:');
+                console.log('~:~:~~: user action:');
                 console.log(data);
-                //socket.broadcast.emit('nt', users);
+                socket.broadcast.emit('user', users);
         });
 });
